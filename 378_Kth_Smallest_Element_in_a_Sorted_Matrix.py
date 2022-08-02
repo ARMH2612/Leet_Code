e562a5a5fd66a3a5b26f5a269900edd81948da10
Houssame Rahiche <houssamerahiche@gmail.com>
2022-08-02 22:18:37 +0100

    Adding challenge number 378: Kth Smallest Element in a Sorted Matrix

---
+++ 378_Kth_Smallest_Element_in_a_Sorted_Matrix.py
@@ -0,0 +1,12 @@
+def kthSmallest(matrix, k):
+    """
+    :type matrix: List[List[int]]
+    :type k: int
+    :rtype: int
+    """
+    # mat = []
+    mat += [n for m in matrix for n in m]
+    print(sorted(mat)[k-1])
+
+
+kthSmallest([[1, 5, 9], [10, 11, 13], [12, 13, 15]], 8)
