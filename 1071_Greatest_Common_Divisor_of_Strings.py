7d0bc71b4e43d2cb59627861a5bad91daade1712
Houssame Rahiche <houssamerahiche@gmail.com>
2024-06-08 19:28:59 +0100

    inhancing speend of 1071. Greatest Common Divisor of Strings

--- 1071_Greatest_Common_Divisor_of_Strings.py
+++ 1071_Greatest_Common_Divisor_of_Strings.py
@@ -9,10 +9,11 @@ def gcdOfStrings(str1: str, str2: str) -> str:
 def GCDS(str1, str2):
     gcds_array = [None]
     for i in range(1,len(str2)+1):
-        gcds = str2[:i] * int(len(str2)/i)
-        if gcds == str2:
-            if(str1 == (str2[:i] * int(len(str1)/i))):
-                gcds_array.append(str2[:i])
+        if len(str2) % len(str2[:i]) == 0:
+            gcds = str2[:i] * int(len(str2)/i)
+            if gcds == str2:
+                if(str1 == (str2[:i] * int(len(str1)/i))):
+                    gcds_array.append(str2[:i])
     
     return gcds_array[-1]
 print(gcdOfStrings( 'LEET', 'CODE' ))
